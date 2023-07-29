6da2943eebf6520eb23625916de7de0fbf62da9d
manblaccc <cayian070802@gmail.com>
2023-07-29 10:33:22 +0900

    pyGame test

--- 2023 07 15.py
+++ 2023 07 15.py
@@ -1,2 +1,22 @@
-print("Hello world")
-print("commit test lol")
+import pygame
+import sys
+from pygame.locals import QUIT
+
+pygame.init()
+Surface = pygame.display.set_mode((600,600))
+FPSCLOCK = pygame.time.Clock()
+pygame.display.set_caption("Test Window")
+
+def main():
+    while True:
+        Surface.fill((255,0,0))
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                sys.exit()
+
+        pygame.display.update()
+        FPSCLOCK.tick(30)
+
+if __name__ == '__main__':
+    main()
