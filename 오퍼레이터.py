7e1a8012d03d7ca9c3ce08a8747103cd2ba22afe
manblaccc <cayian070802@gmail.com>
2023-07-29 11:33:48 +0900

    논리연산 학습

--- 오퍼레이터.py
+++ 오퍼레이터.py
@@ -3,10 +3,34 @@ minus = 10 - 7
 multiply = 20 * 3
 division = 9 / 3
 
-r= 13 % 4
-n = 13 // 4
+r= 13 % 4     #나눗셈 후 나머지 출력
+n = 13 // 4   #나눗셈 후 몫을 출력
 
-print(plus)
-print(minus)
-print(multiply)
-print(division)
+print(r)
+print(n)
+
+
+
+#참 거짓
+trueValue = True     #예약어는 변수로 사용 불가
+
+#논리 연산
+equal = 5 == 4
+notEqual = 5 != 4
+
+print(equal)
+print(notEqual)
+
+test = 13 ** 5 / 17
+print(test)
+
+isBiggerThan = 5 > 5
+isBiggerOrEqualThan = 5 >= 5
+print(isBiggerThan)
+print(isBiggerOrEqualThan)
+
+is_smaller_than = 5 < 5
+is_smaller_or_equal_than = 5 <= 5
+
+print(is_smaller_than)
+print(is_smaller_or_equal_than)
