e47410fc914cfd9987fccbf21c8d3433c1837a3f
manblaccc <cayian070802@gmail.com>
2023-08-26 11:28:39 +0900

    random 값 맞추기

--- whilestatement.py
+++ whilestatement.py
@@ -1,6 +1,6 @@
-number = 0
-
-
+# number = 0
+#
+#
 # while number < 10:
 #     print(number)
 #     number = number + 1
@@ -14,13 +14,27 @@ number = 0
 #
 # while user_input != 2:
 #     user_input = int(input("값을 입력하세요 >>"))
+#
+# while True:
+#     print("=====메뉴=====")
+#     print("1. 시작하기")
+#     print("2. 종료하기")
+#     print("==========")
+#
+#     user_input = int(input("값을 입력하세요 >>"))
+#     if user_input == 2:
+#         break
 
-while True:
-    print("=====메뉴=====")
-    print("1. 시작하기")
-    print("2. 종료하기")
-    print("==========")
 
+import random
+
+answer = random.randrange(0, 10)
+user_input = -1
+# 사용자가 answer 맞출때까지 반복
+
+while user_input != answer:
     user_input = int(input("값을 입력하세요 >>"))
-    if user_input == 2:
+
+    if user_input == answer:
+        print("정답입니다!")
         break
