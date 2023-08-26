1c685e2ecd7f7d60cda1e5fe520d717cf3cfcda4
manblaccc <cayian070802@gmail.com>
2023-08-26 11:12:31 +0900

    while문, break문 학습

---
+++ whilestatement.py
@@ -0,0 +1,15 @@
+number = 0
+
+while number < 10:
+    print(number)
+    number = number + 1
+
+print("=====메뉴=====")
+print("1. 시작하기")
+print("2. 종료하기")
+print("==========")
+
+user_input = -1
+
+while user_input != 2:
+    user_input = int(input("값을 입력하세요 >>"))
