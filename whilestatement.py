5d55f86116ab4f2b57edb835f37e132c72cd20dc
manblaccc <cayian070802@gmail.com>
2023-08-26 11:58:24 +0900

    random 조건 맞추기 문제해결

--- whilestatement.py
+++ whilestatement.py
@@ -30,11 +30,40 @@ import random
 
 answer = random.randrange(0, 10)
 user_input = -1
+trycount = 3
 # 사용자가 answer 맞출때까지 반복
+# 1. 사용자에게 기회주기(3번)
+# 2. 틀렸을때 updown 출력해주기
 
-while user_input != answer:
+# while user_input != answer:
+#     user_input = int(input("값을 입력하세요 >>"))
+#
+#     if user_input == answer:
+#         print("정답입니다!")
+#         break
+
+
+
+while True:
     user_input = int(input("값을 입력하세요 >>"))
 
     if user_input == answer:
         print("정답입니다!")
         break
+
+    elif user_input > answer:
+        print("down")
+        trycount = trycount -1
+
+    elif user_input < answer:
+        print("up")
+        trycount = trycount - 1
+
+
+        if trycount == 0:
+            print("game over")
+            break
+
+
+
+
