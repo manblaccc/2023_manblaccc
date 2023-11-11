8fb83a47cd2c4bfca0f6a823d909d026f6a37b16
manblaccc <cayian070802@gmail.com>
2023-11-11 11:44:40 +0900

    11/11 코드 나누기

---
+++ game/main.py
@@ -0,0 +1,21 @@
+from pythonBasic.game.updown_game import runUpDown
+from pythonBasic.game.hangman_game import runHangMan
+from pythonBasic.game.2048_game import run2048
+
+def menuPrint():
+    print("=======GAME=======")
+    print("1. 행맨")
+    print("2. 업다운")
+    print("0. 종료")
+    print("==================")
+
+userInput = -1
+while userInput != 0:
+    menuPrint()
+    userInput = int(input("SELECT MENU ::: "))
+    if userInput == 1:
+        runHangMan()
+    elif userInput == 2:
+        runUpDown()
+    elif userInput == 3:
+        run2048()
